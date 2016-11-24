62a787fdc2e96671684c487329015ee9b76e8504
taniho_0707 <taniho_0707@yahoo.co.jp>
2016-11-24 12:29:39 +0900

    Fix qtile xscreensaver command

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 feh --bg-scale ~/Pictures/Wallpaper/pc_1366_768.jpg &
 nm-applet &
-xscreensaver-command -no-splash &
+xscreensaver -no-splash &
 xautolock -time 5 -locker 'xscreensaver-command -lock' &
 compton -c -I 0.15 -O 0.2 &
 system-config-printer-applet &
 owncloud &
-# xss-lock -- xscreensaver-command -lock &
+xss-lock -- xscreensaver-command -lock &
 pulseaudio --start
